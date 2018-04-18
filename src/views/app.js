9b99d3e5d64b357ff003b70312ab9a6d656a63e1
krlim417 <krlim417@gmail.com>
2018-04-18 14:52:31 -0700

    Fetch videos when app is initialized and select the first video

--- src/views/app.js
+++ src/views/app.js
@@ -3,15 +3,24 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos(window.exampleVideoData);
+    this.videos = new Videos();
+    
+    this.listenTo(this.videos, 'sync', this.selectFirstVideo);
+    this.videos.search('The Duck Song');
     this.render();
   },
 
+  selectFirstVideo: function() {
+    if (this.videos.length) {
+      this.videos.at(0).select();
+    }
+  },
 
   render: function() {
     this.$el.html(this.template());
 
     new SearchView({
+      collection: this.videos,
       el: this.$('.search')
     }).render();
 
