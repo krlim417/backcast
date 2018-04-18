498b0f62ccb77430307f5c5fc14c468025c7ec12
krlim417 <krlim417@gmail.com>
2018-04-18 12:57:26 -0700

    Render itself, Search view, videoPlayer view, and VideoList view

--- src/views/app.js
+++ src/views/app.js
@@ -3,12 +3,28 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
+    this.videos = new Videos(window.exampleVideoData);
+    this.render();
   },
 
 
   render: function() {
     this.$el.html(this.template());
+
+    new SearchView({
+      el: this.$('.search')
+    }).render();
+
+    new VideoPlayerView({
+      model: this.videos.at(0),
+      collection: this.videos,
+      el: this.$('.player')
+    }).render();
+
+    new VideoListView({
+      collection: this.videos,
+      el: this.$('.list')
+    }).render();
     return this;
   },
 
