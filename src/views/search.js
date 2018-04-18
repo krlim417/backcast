b1e17b1c3d310158eeb25d1bede83498afff21f4
krlim417 <krlim417@gmail.com>
2018-04-18 14:42:32 -0700

    Initiate search when search button is clicked and enter key is pressed

--- src/views/search.js
+++ src/views/search.js
@@ -1,5 +1,23 @@
 var SearchView = Backbone.View.extend({
 
+  events: {
+    "click button": 'initiateSearch',
+    'keyup input': 'handleKeyUp',
+  },
+
+  handleKeyUp: function(event) {
+    if (event.keyCode === 13) {
+      this.initiateSearch();
+    }
+  },
+
+  initiateSearch: function() {
+    const query = this.$('input').val().trim();
+    if (query) {
+      this.collection.search(query);
+    }
+    // this.$('input').val('');
+  },
 
   render: function() {
     this.$el.html(this.template());
