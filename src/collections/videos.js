49d1f1b69904f58d8d02539f0a647a2aeab7fd86
krlim417 <krlim417@gmail.com>
2018-04-18 14:40:50 -0700

    Initiate ajax request when search is called, and convert fetched data to array

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -1,5 +1,23 @@
 var Videos = Backbone.Collection.extend({
 
-  model: Video
+  model: Video,
+
+  search: function(query) {
+    this.fetch({
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      data: {
+        key: window.YOUTUBE_API_KEY,
+        maxResults: 5,
+        part: 'snippet',
+        q: query,
+        type: 'video',
+        videoEmbeddable: 'true'
+      }
+    });
+  },
+
+  parse: function(response) {
+    return response.items;
+  }
 
 });
